18bd01f5be27ee1a3fb9b67b78c6658bcd1e016d
KOTOEWai <149601802+KOTOEWai@users.noreply.github.com>
2025-12-16 22:51:03 +0630

    changeproxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 
 export default withAuth(
-  function middleware(req) {
+  function proxy(req) {
     const token = req.nextauth.token;
     const { pathname } = req.nextUrl;
     // 🚫 1. Block completely if user not logged in
